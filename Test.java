5752eff4a4332c6f592ba96e241e5b1dca3bd610
merrytheressa <merrytheressa@gmail.com>
2025-09-24 10:13:39 +0700

    Menambahkan baris

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("Hai, ini baris kode pertama");
         System.out.println("Hello, ini baris kode kedua");
+        System.out.println("User 2 manambahkan baris");
     }
 }
